c6f2e79c5649305311d8bd5534b6aa943488c888
ru-rocker81@gmail.com <ru-rocker81@gmail.com>
2018-06-15 21:42:23 +0700

    Add micrometer

--- helloservice/src/main/java/com/example/demo/helloservice/controller/HelloController.java
+++ helloservice/src/main/java/com/example/demo/helloservice/controller/HelloController.java
@@ -1,19 +1,26 @@
 package com.example.demo.helloservice.controller;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.annotation.Timed;
 
 
 @RestController
 public class HelloController {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
+	private AtomicInteger atomicInteger = new AtomicInteger();
 	
+	@Timed(value = "get.hello.requests", histogram = true, percentiles = { 0.95, 0.99 }, extraTags = { "version","v1" })
 	@GetMapping(path = "/hello")
 	public String getHello() throws Exception{
-		logger.info("Me! Me! Me!");
-		return "Hello buddy!";
+		int counter = atomicInteger.incrementAndGet();
+		logger.info("My increament: {} ", counter);
+		return "Hello buddy!!";
 	}
 }
