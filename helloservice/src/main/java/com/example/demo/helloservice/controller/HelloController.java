8df57be9e5c04ccecd4236a756505e2bbdd9b6f7
ru-rocker81@gmail.com <ru-rocker81@gmail.com>
2018-06-15 21:43:35 +0700

    add counter

--- helloservice/src/main/java/com/example/demo/helloservice/controller/HelloController.java
+++ helloservice/src/main/java/com/example/demo/helloservice/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo.helloservice.controller;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
+	private AtomicInteger atomicInteger = new AtomicInteger();
 	
 	@GetMapping(path = "/hello")
 	public String getHello() throws Exception{
-		logger.info("Me! Me! Me!");
-		return "Hello buddy!";
+		int counter = atomicInteger.incrementAndGet();
+		logger.info("My increament: {} ", counter);
+		return "Hello buddy!!";
 	}
 }
